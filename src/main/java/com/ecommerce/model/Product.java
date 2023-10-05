1c0abf7cbd58974c3d0391cccae171b87ec0f0f3
sumit7200 <rajsumit7200@gmail.com>
2023-10-05 10:27:48 +0530

    resolve conflict

--- src/main/java/com/ecommerce/model/Product.java
+++ src/main/java/com/ecommerce/model/Product.java
@@ -1,27 +1,11 @@
 package com.ecommerce.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Product {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -45,33 +29,7 @@ public class Product {
     @Column(name = "brand")
     private String brand;
 
-    @Column(name = "colour")
     private String colour;
 
-    @Column(name = "description")
-    private String description;
-
-    @Embedded
-    @ElementCollection
-    @Column(name = "sizes")
-    private Set<Size> sizes = new HashSet<>();
-
-    @Column(name = "image_url")
-    private String imageUrl;
-
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Rating> ratings = new ArrayList<>();
-
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Review> reviews = new ArrayList<>();
-
-    @Column(name = "num_ratings")
-    private int numRatings;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
-
-    private LocalDateTime createAt;
 
 }
