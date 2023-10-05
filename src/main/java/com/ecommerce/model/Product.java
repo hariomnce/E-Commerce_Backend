c6ee392b6ac548646f0eb689f11747839c988d80
Hariom Sharma <hariomne@gmail.com>
2023-10-05 10:41:05 +0530

    resolve conflict

--- src/main/java/com/ecommerce/model/Product.java
+++ src/main/java/com/ecommerce/model/Product.java
@@ -1,35 +1,58 @@
 package com.ecommerce.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "title")
-    private String title;
-
-    @Column(name = "price")
-    private int price;
-
-    @Column(name = "discountedPrice")
-    private int discountedPrice;
-
-    @Column(name = "discountPresent")
-    private int discountPresent;
-
-    @Column(name = "quantity")
-    private int quantity;
-
     @Column(name = "brand")
     private String brand;
 
+    @Column(name = "colour")
     private String colour;
 
+    @Column(name = "description")
+    private String description;
+
+    @Embedded
+    @ElementCollection
+    @Column(name = "sizes")
+    private Set<Size> sizes = new HashSet<>();
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Rating> ratings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    @Column(name = "num_ratings")
+    private int numRatings;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private LocalDateTime createAt;
 
 }
